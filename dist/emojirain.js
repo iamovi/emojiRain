e99610b69382ea7c076ae7b42e93ffecedf5197a
Maruf OVi <137372623+iamovi@users.noreply.github.com>
2024-02-13 14:56:47 +0600

    Update lib

--- dist/emojirain.js
+++ dist/emojirain.js
@@ -1,25 +1,35 @@
 // emoji Rain ------->
 
-// Array of love emojis (without the broken heart)
-const loveEmojis = ['❤️', '💖', '😙', '😙', '😘', '💏', '💑', '💞', '💗', '💓', '💕', '😻', '💘', '💋', '💚', '💜', '🤎', '💛', '🤍', '🧚🏻‍♀️', '🎂', '💫', '🐣', '🐥', '🍰', '😁'];
+const EmojiRain = {
+  // Array of default love emojis
+  defaultLoveEmojis: ['❤️', '💖', '😙', '😙', '😘', '💏', '💑', '💞', '💗', '💓', '💕', '😻', '💘', '💋', '💚', '💜', '🤎', '💛', '🤍', '🧚🏻‍♀️', '🎂', '💫', '🐣', '🐥', '🍰', '😁'],
 
+  // Array of custom love emojis
+  customLoveEmojis: [],
 
-// Function to create and animate falling emojis
-function createFallingEmoji() {
-  const loveEmoji = document.createElement('div');
-  loveEmoji.className = 'love-emoji';
-  loveEmoji.style.left = Math.random() * window.innerWidth + 'px';
-  loveEmoji.textContent = loveEmojis[Math.floor(Math.random() * loveEmojis.length)];
-  document.querySelector('.love-emojis').appendChild(loveEmoji);
-  setTimeout(() => {
-    loveEmoji.remove();
-  }, 5000);
-}
+  // Function to add custom emojis
+  addCustomEmojis: function(emojis) {
+    this.customLoveEmojis.push(...emojis);
+  },
 
-// Function to continuously create falling emojis
-function startFallingEmojis() {
-  setInterval(createFallingEmoji, 1000); // Adjust the interval as needed
-}
+  // Function to create and animate falling emojis
+  createFallingEmoji: function() {
+    const allEmojis = this.customLoveEmojis.length > 0 ? this.customLoveEmojis : this.defaultLoveEmojis;
+    const loveEmoji = document.createElement('div');
+    loveEmoji.className = 'love-emoji';
+    loveEmoji.style.left = Math.random() * window.innerWidth + 'px';
+    loveEmoji.textContent = allEmojis[Math.floor(Math.random() * allEmojis.length)];
+    document.querySelector('.love-emojis').appendChild(loveEmoji);
+    setTimeout(() => {
+      loveEmoji.remove();
+    }, 5000);
+  },
+
+  // Function to continuously create falling emojis
+  startFallingEmojis: function() {
+    setInterval(this.createFallingEmoji.bind(this), 1000); // Adjust the interval as needed
+  }
+};
 
 // Start the falling emojis when the page loads
-window.addEventListener('load', startFallingEmojis);
+window.addEventListener('load', EmojiRain.startFallingEmojis.bind(EmojiRain));
